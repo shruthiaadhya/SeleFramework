aaaba59c8dc1eb283b342be0a64ad5b37a0a1e12
shruthiaadhya <shruthilaya194@gmail.com>
2021-12-16 11:58:54 +0530

    parallel changed

--- src/main/java/com/Vtiger/GenericUtil/BaseClass.java
+++ src/main/java/com/Vtiger/GenericUtil/BaseClass.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
@@ -17,6 +18,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.Vtiger.POMclasses.HomePage;
 import com.Vtiger.POMclasses.LoginPage;
